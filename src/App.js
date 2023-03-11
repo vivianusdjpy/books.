91e17195e68cdbb8e8fd4890c2a6955f3c97920a
VIVIAN NJAU <viviannjau@users.noreply.github.com>
2023-03-11 23:26:10 +0100

    Json server

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+import matchers from '@testing-library/jest-dom/matchers';
 import {useState} from 'react';
 import BookCreate from './components/BookCreate';
+import BookList from './components/BookList';
 
 function App() {
     const [books,setBooks] = useState([]);
-    
-    const createBook = (title) => {
-        console.log('Need to add book with:', title);
+
+    const editBookById = (id, newTitle) => {
+        const updatedBooks = books.map((book) => {
+            if (book.id === id) {
+                return { ...book, title: newTitle };
+            }
+            return book;
+        });
+        setBooks(updatedBooks);
     };
 
-    return <div>
+    const deleteBookById = (id) => {
+        const updatedBooks = books.filter((book) => {
+            return book.id !==id;
+        });
+        setBooks(updatedBooks);
+    };
+    
+    const createBook = (title) => {
+        const updatedBooks = [...books, {
+            id: Math.round(Math.random() * 9999),
+            title } ];
+        setBooks(updatedBooks);
+    };
+
+    return (<div className='app'>
+        <h1>Reading List</h1>
+        <BookList onEdit={editBookById} books={books} onDelete={deleteBookById}/>
         <BookCreate onCreate = {createBook}/>
-    </div>;
+    </div>
+    );
 }
 
 export default App;
